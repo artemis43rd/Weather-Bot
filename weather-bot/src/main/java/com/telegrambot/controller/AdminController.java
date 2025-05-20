e4783158335c1a0a214233297d17023a49a544ef
acherdancev <acherdancev@radiofid.ru>
2025-05-20 16:34:47 +0300

    admin: fix Unauthorized error, add admin pass

--- weather-bot/src/main/java/com/telegrambot/controller/AdminController.java
+++ weather-bot/src/main/java/com/telegrambot/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.telegrambot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.telegrambot.model.User;
 import com.telegrambot.service.AdminService;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/admin")
@@ -24,5 +29,14 @@ public class AdminController {
     @GetMapping("/users")
     public List<User> getUsers(@RequestParam("password") String password) {
         return adminService.getUsersIfAdmin(password);
+    }
+
+    @ExceptionHandler(SecurityException.class)
+    public ResponseEntity<Map<String, String>> handleSecurityException(SecurityException ex) {
+        Map<String, String> errorResponse = Map.of(
+            "error", "Authentication Failed",
+            "message", "Incorrect password!"
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 }
